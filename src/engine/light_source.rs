e138e90213df20faddf740d924420af770e379f2
Ramos-notebook <gu.ramos@proton.me>
2024-10-29 22:14:03 -0300

    Mudei as cores do light_source pra serem em vec3 ao invés de color

--- src/engine/light_source.rs
+++ src/engine/light_source.rs
@@ -1,16 +1,15 @@
 #![allow(dead_code)]
 use crate::utils::vec::Vec3;
-use sdl2::pixels::Color;
 
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub struct LightSource {
     pub pos: Vec3,
-    pub color: Color,
+    pub color: Vec3,
     pub intensity: f32
 }
 
 impl LightSource {
-    pub fn new(pos: Vec3, color: Color, intensity: f32) -> LightSource {
+    pub fn new(pos: Vec3, color: Vec3, intensity: f32) -> LightSource {
         LightSource { pos, color, intensity }
     }
 }
