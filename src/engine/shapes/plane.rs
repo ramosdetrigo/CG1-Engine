f06704e34db7cc87502aaee598ab4bc2180c9aa4
Ramos-notebook <gu.ramos@proton.me>
2024-11-15 13:59:58 -0300

    Changed plane p0 to pc

--- src/engine/shapes/plane.rs
+++ src/engine/shapes/plane.rs
@@ -5,9 +5,9 @@ use super::super::Ray;
 use crate::utils::Vec3;
 
 #[derive(Clone, PartialEq)]
-/// Plano baseado num ponto `p0` com vetor normal `normal`, de material `material`.
+/// Plano baseado num ponto `pc` com vetor normal `normal`, de material `material`.
 pub struct Plane {
-    pub p0: Vec3,
+    pub pc: Vec3, // ponto conhecido do plano
     pub normal: Vec3,
     pub material: Material,
 }
@@ -16,10 +16,10 @@ pub struct Plane {
 impl Plane {
     #[inline]
     #[must_use]
-    /// Cria um novo plano baseado num ponto `p0` com vetor normal `normal`, de material `material`. \
+    /// Cria um novo plano baseado num ponto `pc` com vetor normal `normal`, de material `material`. \
     /// (Encapsulado em um enum Shape)
-    pub fn new(p0: Vec3, normal: Vec3, material: Material ) -> Shape {
-        Shape::Plane( Self { p0, normal, material } )
+    pub fn new(pc: Vec3, normal: Vec3, material: Material ) -> Shape {
+        Shape::Plane( Self { pc, normal, material } )
     }
 
     #[inline]
@@ -27,13 +27,13 @@ impl Plane {
     /// Retorna o ponto de interseção (de distância positiva) mais próximo entre um plano e um raio `r` \
     /// (`-INFINITY` se não há interseção)
     pub fn intersects(&self, r: &Ray) -> f32 {
-        // Fórmula: n * (p - p0) = 0
-        // n * (R(t) - p0) = 0
-        // t = - n.dot(r.origin - p0) / n.dot(r.dr)
-        // onde p0 é o ponto "central" do plano e n é o vetor normal do plano
+        // Fórmula: n * (p - pc) = 0
+        // n * (R(t) - pc) = 0
+        // t = - n.dot(r.origin - pc) / n.dot(r.dr)
+        // onde pc é o ponto "central" do plano e n é o vetor normal do plano
         // se n.dot(r.dr) é 0, os raio é paralelo ao plano, retorna falso (-INFINITO)
         // se não, retorna o resultado da fórmula.
-        let top = self.normal.dot(r.origin - self.p0);
+        let top = self.normal.dot(r.origin - self.pc);
         let bottom = self.normal.dot(r.dr);
         if bottom == 0.0 { return NEG_INFINITY }
         -top/bottom
